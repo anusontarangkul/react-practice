0af4487b5be6dfad51dce23dc07963aaf1e619c3
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-01-20 16:06:40 -0800

    add author component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -15,6 +15,7 @@ const Book = () => {
         <article>
             <Image />
             <Title />
+            <Author />
         </article>
     );
 }
@@ -22,6 +23,7 @@ const Book = () => {
 const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/81RcyFo8h5L.__BG0,0,0,0_FMpng_AC_UL240_SR240,240_.jpg" alt="book preview" />
 
 const Title = () => <h1>Across The Green Grass Fields</h1>
+const Author = () => <h4>Seanan McGuire</h4>
 
 
 ReactDom.render(<BookList />, document.getElementById('root'))
